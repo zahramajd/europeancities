c7f5d0a1171f4b2652b88a25acc5f0d3af494778
zahramajd <zahra.majdabadi95@gmail.com>
2017-07-24 15:11:06 +0430

    first commit

---
+++ pages/Post.vue
@@ -0,0 +1,80 @@
+<template>
+    <section class="container">
+        <div>
+    
+            <div>
+                <b-button-group>
+                    <b-button active>
+                        <router-link to="/index">main</router-link>
+                    </b-button>
+                    <b-button>
+                        <router-link to="/Post">post</router-link>
+                    </b-button>
+                    <b-button>
+                        <router-link to="/Category">category</router-link>
+                    </b-button>
+                    <b-button>
+                        <router-link to="/Search">search</router-link>
+                    </b-button>
+                </b-button-group>
+    
+                <br>
+                <br>
+    
+                <b-card img="https://media1.britannica.com/eb-media/20/191120-004-AD2A2694.jpg" title="It's Austria" class="mb-2">
+                    I wanna go there
+                </b-card>
+    
+                <div>
+                    {{title}}
+                </div>
+            </div>
+    
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    async asyncData({ app }) {
+        let { posts } = await app.$axios.get('http://www.europeancitiesmarketing.com/wp-json/wp/v2/posts').data
+        return {
+            title: posts[1].status
+        }
+    }
+}
+
+</script>
+
+<style>
+.container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
+.title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+}
+
+.subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+}
+
+.links {
+    padding-top: 15px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="container">
+    <div>
+  
+      <div>
+        <b-button-group>
+          <b-button active>
+            <router-link to="/index">main</router-link>
+          </b-button>
+          <b-button>
+            <router-link to="/Post">post</router-link>
+          </b-button>
+          <b-button>
+            <router-link to="/Category">category</router-link>
+          </b-button>
+          <b-button>
+            <router-link to="/Search">search</router-link>
+          </b-button>
+        </b-button-group>
+  
+        <br>
+        <br>
+  
+        <b-card img="https://media1.britannica.com/eb-media/20/191120-004-AD2A2694.jpg" title="It's Austria" class="mb-2">
+          I wanna go there
+        </b-card>
+  
+        <div>
+          this is the main page of my blog, your ip is {{ip}}
+        </div>
+      </div>
+  
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  async asyncData({ app }) {
+    const { data } = await app.$axios.get('http://icanhazip.com')
+    return {
+      ip: data
+    }
+  }
+}
+
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
